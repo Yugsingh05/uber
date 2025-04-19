f3bba7295bea8b77620cf1aeae7c0cf6a9932134
Yugsingh05 <yugsingh7622@gmail.com>
2025-04-19 18:30:11 +0530

    feat : Added the bottom bar

--- app/(root)/(tabs)/_layout.tsx
+++ app/(root)/(tabs)/_layout.tsx
@@ -1,20 +1,114 @@
+import { icons } from "@/constants";
 import { useAuth } from "@clerk/clerk-expo";
-import { Redirect, Stack } from "expo-router";
+import { Redirect, Stack, Tabs } from "expo-router";
+import { Image, ImageSourcePropType, View } from "react-native";
+
+const TabIcon = ({
+  source,
+  focused,
+}: {
+  source: ImageSourcePropType;
+  focused: boolean;
+}) => (
+  <View
+    className={`flex flex-row justify-center items-center rounded-full ${focused ? "bg-general-200" : ""}`}
+  >
+    <View
+      className={`rounded-full w-12 h-12 items-center justify-center ${focused ? "bg-general-400" : ""}`}
+    >
+      <Image
+        source={source}
+        tintColor="white"
+        resizeMode="contain"
+        className="w-7 h-7"
+      />
+    </View>
+  </View>
+);
+
 
 export default function Layout() {
-
-  const { isSignedIn } = useAuth()
+  const { isSignedIn } = useAuth();
 
   if (!isSignedIn) {
-    return <Redirect href={'/sign-in'} />
+    return <Redirect href={"/sign-in"} />;
   }
   return (
-    <Stack>
-      <Stack.Screen name="HomeScreen" options={{ headerShown: false, headerBackVisible: false, headerTransparent: true }} />
-      <Stack.Screen name="Profile" options={{ headerShown: false, headerBackVisible: false, headerTransparent: true }} />
-      <Stack.Screen name="Chat" options={{ headerShown: false, headerBackVisible: false, headerTransparent: true }} />
-      <Stack.Screen name="Rides" options={{ headerShown: false, headerBackVisible: false, headerTransparent: true }} />
-      
-    </Stack>
+    <Tabs
+      initialRouteName="HomeScreen"
+      screenOptions={{
+        tabBarActiveTintColor: "white",
+        tabBarInactiveTintColor: "white",
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          backgroundColor: "#333333",
+          borderRadius: 50,
+          paddingBottom: 25, // ios only
+          overflow: "hidden",
+          marginHorizontal: 20,
+          marginBottom: 20,
+          height: 78,
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          flexDirection: "row",
+          position: "absolute",
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="HomeScreen"
+        options={{
+          title: "Home",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon source={icons.home} focused={focused} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Rides"
+        options={{
+          title: "Rides",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon source={icons.list} focused={focused} />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="Chat"
+        options={{
+          title: "Chat",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon source={icons.chat} focused={focused} />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="Profile"
+        options={{
+          title: "Profile",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon source={icons.profile} focused={focused} />
+          ),
+        }}
+      />
+    </Tabs>
   );
+}
+
+{
+  /* <Stack>
+//     <Stack.Screen name="HomeScreen" options={{ headerShown: false, headerBackVisible: false, headerTransparent: true }} />
+//     <Stack.Screen name="Profile" options={{ headerShown: false, headerBackVisible: false, headerTransparent: true }} />
+//     <Stack.Screen name="Chat" options={{ headerShown: false, headerBackVisible: false, headerTransparent: true }} />
+//     <Stack.Screen name="Rides" options={{ headerShown: false, headerBackVisible: false, headerTransparent: true }} />
+    
+//   </Stack>
+// ); */
 }
