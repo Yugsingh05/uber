19d351184fb9cf37496814a104179ab1c168d42b
Yugsingh05 <yugsingh7622@gmail.com>
2025-04-19 15:36:32 +0530

    feat : Colpeted till verfied modal

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,6 +1,13 @@
-import { Stack } from "expo-router";
+import { useAuth } from "@clerk/clerk-expo";
+import { Redirect, Stack } from "expo-router";
 
 export default function Layout() {
+
+  const { isSignedIn } = useAuth()
+
+  if (isSignedIn) {
+    return <Redirect href={'/(root)/(tabs)/HomeScreen'} />
+  }
   return (
     <Stack>
       <Stack.Screen name="welcome" options={{ headerShown: false, headerBackVisible: false, headerTransparent: true }} />
--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -2,19 +2,58 @@ import CustomButton from "@/components/CustomButton";
 import InputField from "@/components/InputField";
 import OAuth from "@/components/OAuth";
 import { icons, images } from "@/constants";
-import { Link } from "expo-router";
+import { useSignUp } from "@clerk/clerk-expo";
+import { Link, useRouter } from "expo-router";
 import React, { useState } from "react";
-import { Image, ScrollView, Text, View } from "react-native";
+import { Alert, Image, ScrollView, Text, TextInput, View } from "react-native";
+import {ReactNativeModal} from 'react-native-modal'
 
 const SignIn = () => {
   const [name, setName] = useState("");
-
+  const { isLoaded, signUp, setActive } = useSignUp();
+  const router = useRouter();
+  const [pendingVerification, setPendingVerification] = useState(false);
+  const [isSuccess, setIsSuccess] = useState(true);
+  const [code, setCode] = useState("");
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
 
-  const handleSignIn = () => {
-    console.log(name, email, password);
-  }
+  const onSignUpPress = async () => {
+    if (!isLoaded) return;
+
+    // Start sign-up process using email and password provided
+    try {
+      await signUp.create({
+        emailAddress: email,
+        password,
+      });
+
+      await signUp.prepareEmailAddressVerification({ strategy: "email_code" });
+      setPendingVerification(true);
+    } catch (err) {
+      console.error(JSON.stringify(err, null, 2));
+    }
+  };
+
+  const onVerifyPress = async () => {
+    if (!isLoaded) return;
+
+    try {
+      const signUpAttempt = await signUp.attemptEmailAddressVerification({
+        code,
+      });
+
+      if (signUpAttempt.status === "complete") {
+        await setActive({ session: signUpAttempt.createdSessionId });
+        router.replace("/");
+      } else {
+        console.error(JSON.stringify(signUpAttempt, null, 2));
+      }
+    } catch (err) {
+      console.error(JSON.stringify(err, null, 2));
+      Alert.alert("Error", JSON.stringify(err, null, 2));
+    }
+  };
 
   return (
     <ScrollView className="flex-1 bg-white">
@@ -52,18 +91,40 @@ const SignIn = () => {
             onChangeText={setPassword}
           />
 
-          <CustomButton title="sign in" onPress={handleSignIn} className="mt-6"/>
+          <CustomButton
+            title="sign in"
+            onPress={onSignUpPress}
+            className="mt-6"
+          />
 
-          <OAuth/>
+          <OAuth />
 
-          <Link href={'/sign-up'} className="text-lg text-center text-general-200 mt-10">
-          <Text>Already have an account?</Text>
-          <Text className="text-primary-500">Log In</Text>
+          <Link
+            href={"/sign-up"}
+            className="text-lg text-center text-general-200 mt-10"
+          >
+            <Text>Already have an account?</Text>
+            <Text className="text-primary-500">Log In</Text>
           </Link>
-
-
-
         </View>
+
+        <ReactNativeModal isVisible={isSuccess}>
+          <View className="bg-white px-7 py-9 rounded-2xl min-h-[300px]">
+            <Image
+            source={images.check} className="w-[110px] h-[110px] mx-auto my-5"/>
+
+            <Text className="text-2xl font-JakartaSemiBold text-black text-center">
+              Account Verfied
+            </Text>
+
+            <Text className="text-lg font-JakartaRegular text-black text-center">
+              You have successfully created your account
+            </Text>
+
+
+          </View>
+
+        </ReactNativeModal>
       </View>
     </ScrollView>
   );
